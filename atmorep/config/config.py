5d61bd0a85f8a16c9d82028b6bd4fcde2f2fdcd1
Christian Lessig <christian.lessig@ovgu.de>
2023-12-30 11:18:19 +0100

    Added dataset infos (resolution, extent, ...) to config and the information from there is used in normalizer_locals. In other places it still needs to be changed that the info is used (including in train.py; no need to specify extent of cosmo dataset there).

--- atmorep/config/config.py
+++ atmorep/config/config.py
@@ -7,7 +7,7 @@ year_base = 1979
 year_last = 2021
 
 path_models = Path( fpath, '../../models/')
-path_results = Path( fpath, '../../results/')
+path_results = Path( fpath, '../../results')
 path_data = Path( fpath, '../../data/')
 path_plots = Path( fpath, '../results/plots/')
 
@@ -17,3 +17,24 @@ grib_index = { 'vorticity' : 'vo', 'divergence' : 'd', 'geopotential' : 'z',
                 'velocity_u' : 'u', 'velocity_v': 'v', 'velocity_z' : 'w',
                 'total_precip' : 'tp', 'radar_precip' : 'yw_hourly',
                 't2m' : 't_2m', 'u_10m' : 'u_10m', 'v_10m' : 'v_10m',  }
+
+# TODO: extract this info from the datasets
+datasets = {}
+#
+datasets['era5'] = {}
+datasets['era5']['resolution'] = [1, 0.25, 0.25]
+datasets['era5']['extent'] = [ [1979, 2022], [90., -90], [0.0, 360] ]
+datasets['era5']['is_global'] = True
+datasets['era5']['file_size'] = [ -1, 721, 1440]
+#
+datasets['cosmo_rea6'] = {}
+datasets['cosmo_rea6']['resolution'] = [1, 0.0625, 0.0625]
+datasets['cosmo_rea6']['extent'] = [ [1997, 2017], [27.5,70.25], [-12.5,37.0] ]
+datasets['cosmo_rea6']['is_global'] = False
+datasets['cosmo_rea6']['file_size'] = [ -1, 685, 793]
+#
+datasets['cerra'] = {}
+datasets['cerra']['resolution'] = [3, 0.25, 0.25]
+datasets['cerra']['extent'] = [ [1985, 2001], [75.25,20.5], [-58.0,74.0] ]
+datasets['cerra']['is_global'] = False
+datasets['cerra']['file_size'] = [ -1, 220, 529]
--- atmorep/datasets/normalizer_local.py
+++ atmorep/datasets/normalizer_local.py
@@ -24,33 +24,29 @@ class NormalizerLocal() :
 
   def __init__(self, field_info, vlevel, file_shape, data_type = 'era5', level_type = 'ml') :
 
-    if 'cosmo_rea6' == data_type :
-      path = ''
-      fname_base = './data/{}/normalization/{}/normalization_mean_var_{}_y{}_m{:02d}_{}{}.bin'
-      self.year_base = 1995
-      self.year_last = 2017
-      lat_min, lat_max = 90. - 70.25, 90. - 27.5
-      lon_min, lon_max = -12.5, 37.0
-      is_global = False
-      res = 0.25 / 4
-    else :
-      path = str(config.path_data)
-      fname_base = './data/{}/normalization/{}/normalization_mean_var_{}_y{}_m{:02d}_{}{}.bin'
-      self.year_base = config.year_base
-      self.year_last = config.year_last
-      lat_min, lat_max = 0., 180.
-      lon_min, lon_max = 0., 360.
-      res = 0.25
-      is_global = True
+    fname_base = './data/{}/normalization/{}/normalization_mean_var_{}_y{}_m{:02d}_{}{}.bin'
+    self.year_base = config.datasets[data_type]['extent'][0][0]
+    self.year_last = config.datasets[data_type]['extent'][0][1]
+    lat_min, lat_max = config.datasets[data_type]['extent'][1]
+    lat_min, lat_max = 90. - lat_min, 90. - lat_max
+    lat_min, lat_max = (lat_min, lat_max) if lat_min < lat_max else (lat_max, lat_min)
+    lon_min, lon_max = config.datasets[data_type]['extent'][2]
+    res = config.datasets[data_type]['resolution'][1]
+    is_global = config.datasets[data_type]['is_global']
 
     self.corr_data = [ ]
     for year in range( self.year_base, self.year_last+1) :
       for month in range( 1, 12+1) :
         corr_fname = fname_base.format( data_type, field_info[0], field_info[0],
                                         year, month, level_type, vlevel)
-        ns_lat = int( (lat_max-lat_min) / res + 1)  # (1 if is_global else 0) )
+        ns_lat = int( (lat_max-lat_min) / res + 1)
         ns_lon = int( (lon_max-lon_min) / res + (0 if is_global else 1) )
+        # TODO: remove file_shape (ns_lat, ns_lon contains same information)
         x = np.fromfile( corr_fname, dtype=np.float32).reshape( (file_shape[1], file_shape[2], 2))
+        # TODO, TODO, TODO: remove once recomputed
+        if 'cerra' == data_type :
+          x[:,:,0] = 340.
+          x[:,:,1] = 600.
         x = xr.DataArray( x, [ ('lat', np.linspace( lat_min, lat_max, num=ns_lat, endpoint=True)),
                                 ('lon', np.linspace( lon_min, lon_max, num=ns_lon, endpoint=False)),
                                ('data', ['mean', 'var']) ])
