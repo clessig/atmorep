0e25a979a780e7d7ad15d73271f309f293eebfb1
Ilaria Luise <ilaria.luise@cern.ch>
2024-08-12 17:33:35 +0200

    rebase to main and create train_multi

--- atmorep/core/train_multi.py
+++ atmorep/core/train_multi.py
@@ -20,6 +20,7 @@ import os
 
 import wandb
 
+import atmorep.config.config as config
 from atmorep.core.trainer import Trainer_BERT
 from atmorep.utils.utils import Config
 from atmorep.utils.utils import setup_ddp
@@ -27,22 +28,23 @@ from atmorep.utils.utils import setup_wandb
 from atmorep.utils.utils import init_torch
 import atmorep.utils.utils as utils
 
-import atmorep.config.config as config
 
 ####################################################################################################
-def train_continue( model_id, model_epoch, Trainer, model_epoch_continue = -1) :
+def train_continue( wandb_id, epoch, Trainer, epoch_continue = -1) :
 
   num_accs_per_task = int( 4 / int( os.environ.get('SLURM_TASKS_PER_NODE', '1')[0] ))
   device = init_torch( num_accs_per_task)
   with_ddp = True
   par_rank, par_size = setup_ddp( with_ddp)
 
-  cf = Config().load_json( model_id)
+  cf = Config().load_json( wandb_id)
+
   cf.with_ddp = with_ddp
   cf.par_rank = par_rank
   cf.par_size = par_size
+  cf.optimizer_zero = False
   cf.attention = False
-  cf.optimizer_zero = False   
+  # name has changed but ensure backward compatibility
   if hasattr( cf, 'loader_num_workers') :
     cf.num_loader_workers = cf.loader_num_workers
   if not hasattr( cf, 'n_size'):
@@ -53,89 +55,93 @@ def train_continue( model_id, model_epoch, Trainer, model_epoch_continue = -1) :
     cf.num_samples_validate = 128
   if not hasattr(cf, 'with_mixed_precision'):
     cf.with_mixed_precision = True
+  
+  cf.years_train = [2021] # list( range( 1980, 2018))
+  cf.years_val = [2021]  #[2018] 
+  
+  # any parameter in cf can be overwritten when training is continued, e.g. we can increase the 
+  # masking rate 
+  # cf.fields = [ [ 'specific_humidity', [ 1, 2048, [ ], 0 ], 
+  #                               [ 96, 105, 114, 123, 137 ], 
+  #                               [12, 6, 12], [3, 9, 9], [0.5, 0.9, 0.1, 0.05] ] ]
 
-  setup_wandb( cf.with_wandb, cf, par_rank, 'train-multi', mode='offline')  
-
+  setup_wandb( cf.with_wandb, cf, par_rank, project_name='train', mode='offline')  
+  # resuming a run requires online mode, which is not available everywhere
+  #setup_wandb( cf.with_wandb, cf, par_rank, wandb_id = wandb_id)  
+  
   if cf.with_wandb and 0 == cf.par_rank :
     cf.write_json( wandb)
     cf.print()
 
-  if -1 == model_epoch_continue :
-    model_epoch_continue = model_epoch
+  if -1 == epoch_continue :
+    epoch_continue = epoch
 
   # run
-  trainer = Trainer.load( cf, model_id, model_epoch, device)
-  print( 'Loaded run \'{}\' at epoch {}.'.format( model_id, model_epoch))
-  trainer.run( model_epoch_continue)
+  trainer = Trainer.load( cf, wandb_id, epoch, device)
+  print( 'Loaded run \'{}\' at epoch {}.'.format( wandb_id, epoch))
+  trainer.run( epoch_continue)
 
 ####################################################################################################
-def train_multi() :
+def train() :
 
   num_accs_per_task = int( 4 / int( os.environ.get('SLURM_TASKS_PER_NODE', '1')[0] ))
   device = init_torch( num_accs_per_task)
+  # device = ['cuda']
   with_ddp = True
   par_rank, par_size = setup_ddp( with_ddp)
 
+  # torch.cuda.set_sync_debug_mode(1)
+  torch.backends.cuda.matmul.allow_tf32 = True
+
   cf = Config()
-  # horovod 
-  cf.num_accs_per_task = num_accs_per_task   # number of GPUs / accelerators per task
+  # parallelization
   cf.with_ddp = with_ddp
+  cf.num_accs_per_task = num_accs_per_task   # number of GPUs / accelerators per task
   cf.par_rank = par_rank
   cf.par_size = par_size
-  cf.back_passes_per_step = 4
-  # general
-  cf.comment = ''
-  cf.file_format = 'grib'
-  cf.file_path = str(config.path_data)
-  cf.level_type = 'ml'
   
-  cf.fields = [ [ 'vorticity', [ 1, 2048, ['divergence', 'temperature'], 0 ], 
-                                [ 96, 105, 114, 123, 137 ], 
-                                [12, 6, 12], [3, 9, 9], [0.25, 0.9, 0.2, 0.05] ],
-                [ 'velocity_z', [ 1, 1536, ['vorticity', 'divergence'], 0 ], 
-                                [ 96, 105, 114, 123, 137 ], 
-                                [12, 6, 12], [3, 9, 9], [0.25, 0.9, 0.2, 0.05] ], 
-                [ 'divergence', [ 1, 2048, ['vorticity', 'temperature'], 1 ], 
-                                [ 96, 105, 114, 123, 137 ], 
-                                [12, 6, 12], [3, 9, 9], [0.25, 0.9, 0.2, 0.05] ],
-                [ 'specific_humidity', [ 1, 2048, ['vorticity', 'divergence', 'velocity_z'], 2 ], 
-                                [ 96, 105, 114, 123, 137 ], 
-                                [12, 6, 12], [3, 9, 9], [0.25, 0.9, 0.2, 0.05] ],
-                [ 'temperature', [ 1, 1024, ['vorticity', 'divergence'], 3 ], 
-                                [ 96, 105, 114, 123, 137 ], 
-                                [12, 6, 12], [3, 9, 9], [0.25, 0.9, 0.2, 0.05] ],   
-                [ 'total_precip', [ 1, 1536, ['vorticity', 'divergence', 'specific_humidity'], 3 ],
-                                  [ 0 ], 
-                                  [12, 6, 12], [3, 9, 9], [0.25, 0.9, 0.2, 0.05] ] ]
-  cf.fields_prediction = [['vorticity', 0.25],  ['velocity_z', 0.1], 
-                          ['divergence', 0.25], ['specific_humidity', 0.15],
-                          ['temperature', 0.15], ['total_precip', 0.1] ]
+  # format: list of fields where for each field the list is 
+  # [ name , 
+  #   [ dynamic or static field { 1, 0 }, embedding dimension, , device id ],
+  #   [ vertical levels ],
+  #   [ num_tokens],
+  #   [ token size],
+  #   [ total masking rate, rate masking, rate noising, rate for multi-res distortion]
+  # ]
 
-  # cf.fields = [ [ 'velocity_u', [ 1, 2048, ['velocity_v', 'temperature'], 0, ['dys79lgw', 82] ], 
+  # cf.fields = [ [ 'temperature',[ 1, 512, [ ], 0 ], 
   #                               [ 96, 105, 114, 123, 137 ], 
-  #                               [12, 6, 12], [3, 9, 9], [0.5, 0.9, 0.2, 0.05], 'local' ],
-  #               [ 'velocity_v', [ 1, 2048, ['velocity_u', 'temperature'], 1, ['22j6gysw', 82] ], 
-  #                               [ 96, 105, 114, 123, 137 ], 
-  #                               [12, 6, 12], [3, 9, 9], [0.5, 0.9, 0.2, 0.05], 'local' ], 
-  #               [ 'specific_humidity', [ 1, 2048, ['velocity_u', 'velocity_v', 'temperature'], 2, ['1565pb1f', 62] ], 
-  #                             [ 96, 105, 114, 123, 137 ], 
-  #                             [12, 6, 12], [3, 9, 9], [0.5, 0.9, 0.2, 0.05], 'local' ],
-  #               [ 'velocity_z', [ 1, 1024, ['velocity_u', 'velocity_v', 'temperature'], 3, ['3cb4q6sl', 190] ], 
-  #                             [ 96, 105, 114, 123, 137 ], 
-  #                             [12, 6, 12], [3, 9, 9], [0.5, 0.9, 0.2, 0.05], 'global' ],
-  #               [ 'temperature', [ 1, 1024, ['velocity_u', 'velocity_v', 'specific_humidity'], 3, ['2147fkco', 87] ], 
-  #                             [ 96, 105, 114, 123, 137 ], 
-  #                             [12, 6, 12], [3, 9, 9], [0.5, 0.9, 0.2, 0.05], 'local' ],
-  #               ['total_precip', [1, 1536, ['velocity_u', 'velocity_v', 'velocity_z', 'specific_humidity'], 3, ['3kdutwqb', 900]], 
-  #                             [0], 
-  #                             [12, 6, 12], [3, 9, 9], [0.25, 0.9, 0.1, 0.05]] ]
-  # cf.fields_prediction = [['velocity_u', 0.225], ['velocity_v', 0.225], 
-  #                         ['specific_humidity', 0.15], ['velocity_z', 0.1], ['temperature', 0.2],
-  #                         ['total_precip', 0.1] ]
+  #                               [12, 2, 4], [3, 27, 27], [0.25, 0.9, 0.2, 0.05], 'local' ]  ]
+ 
+  # cf.fields_prediction = [ [cf.fields[0][0], 1.] ]
+
+  cf.fields = [ [ 'velocity_u', [ 1, 2048, ['velocity_v', 'temperature'], 0 ], 
+                                [ 96, 105, 114, 123, 137 ], 
+                                [12, 6, 12], [3, 9, 9], [0.5, 0.9, 0.2, 0.05], 'local' ],
+                [ 'velocity_v', [ 1, 2048, ['velocity_u', 'temperature'], 1 ], 
+                                [ 96, 105, 114, 123, 137 ], 
+                                [12, 6, 12], [3, 9, 9], [0.5, 0.9, 0.2, 0.05], 'local' ], 
+                [ 'specific_humidity', [ 1, 2048, ['velocity_u', 'velocity_v', 'temperature'], 2 ], 
+                              [ 96, 105, 114, 123, 137 ], 
+                              [12, 6, 12], [3, 9, 9], [0.5, 0.9, 0.2, 0.05], 'local' ],
+                [ 'velocity_z', [ 1, 1024, ['velocity_u', 'velocity_v', 'temperature'], 3 ], 
+                              [ 96, 105, 114, 123, 137 ], 
+                              [12, 6, 12], [3, 9, 9], [0.5, 0.9, 0.2, 0.05], 'global' ],
+                [ 'temperature', [ 1, 1024, ['velocity_u', 'velocity_v', 'specific_humidity'], 3 ], 
+                              [ 96, 105, 114, 123, 137 ], 
+                              [12, 6, 12], [3, 9, 9], [0.5, 0.9, 0.2, 0.05], 'local' ],
+                ['total_precip', [1, 1536, ['velocity_u', 'velocity_v', 'velocity_z', 'specific_humidity'], 3], 
+                              [0], 
+                              [12, 6, 12], [3, 9, 9], [0.25, 0.9, 0.1, 0.05]] ]
+ 
+  cf.fields_prediction = [['velocity_u', 0.225], ['velocity_v', 0.225], 
+                          ['specific_humidity', 0.15], ['velocity_z', 0.1], ['temperature', 0.2],
+                          ['total_precip', 0.1] ]
 
   cf.fields_targets = []
-  cf.years_train = [2021] #list( range( 1980, 2018))
-  cf.years_test = [2021] #[2018] 
+
+  cf.years_train = list( range( 2010, 2021))
+  cf.years_val = [2021]  #[2018] 
   cf.month = None
   cf.geo_range_sampling = [[ -90., 90.], [ 0., 360.]]
   cf.time_sampling = 1   # sampling rate for time steps
@@ -143,19 +149,17 @@ def train_multi() :
   cf.data_smoothing = 0
   cf.file_shape = (-1, 721, 1440)
   cf.num_t_samples = 31*24
-  cf.num_patches_per_t_train = 4
-  cf.num_patches_per_t_test = 1
+  cf.num_files_train = 5
+  cf.num_files_test = 2
+  cf.num_patches_per_t_train = 8
+  cf.num_patches_per_t_test = 4
   # random seeds
   cf.torch_seed = torch.initial_seed()
   # training params
-  cf.batch_size_test = 8
-  cf.batch_size_start = 2
-  cf.batch_size_max = 2
-  cf.batch_size_delta = 1
+  cf.batch_size_validation = 1 #64
+  cf.batch_size = 8 #16 #4 # 32
   cf.num_epochs = 128
-  cf.num_files_train = 1
-  cf.num_files_test = 1 # int( np.ceil( 12. / par_size ))
-  cf.num_loader_workers = 2
+  
   # additional infos
   cf.size_token_info = 8
   cf.size_token_info_net = 16
@@ -163,18 +167,17 @@ def train_multi() :
   cf.with_cls = False
   # network config
   cf.with_layernorm = True
-  cf.coupling_num_heads_per_field = 2
+  cf.coupling_num_heads_per_field = 1
   cf.dropout_rate = 0.05
   cf.learnable_mask = False
-  cf.with_qk_lnorm = True
-  cf.num_neighborhoods = 1
+  cf.with_qk_lnorm = False
   # encoder
-  cf.encoder_num_layers = 10
+  cf.encoder_num_layers = 6 #10 #4
   cf.encoder_num_heads = 16
   cf.encoder_num_mlp_layers = 2
   cf.encoder_att_type = 'dense'
   # decoder
-  cf.decoder_num_layers = 10
+  cf.decoder_num_layers = 6 #10 #4
   cf.decoder_num_heads = 16
   cf.decoder_num_mlp_layers = 2
   cf.decoder_self_att = False
@@ -184,24 +187,25 @@ def train_multi() :
   # tail net
   cf.net_tail_num_nets = 16
   cf.net_tail_num_layers = 0
-  cf.ensemble_weighted = False
-  cf.ensemble_weighted_T = 50.
   # loss
   # supported: see Trainer for supported losses
   # cf.losses = ['mse', 'stats']
   cf.losses = ['mse_ensemble', 'stats']
+  # cf.losses = ['mse']
   # cf.losses = ['stats']
+  # cf.losses = ['crps']
   # training
   cf.optimizer_zero = False
-  cf.lr_start = 0.00001
-  cf.lr_max = 0.00002
-  cf.lr_min = 0.00001
-  cf.weight_decay = 0.025
+  cf.lr_start = 5. * 10e-7
+  cf.lr_max = 0.00005
+  cf.lr_min = 0.00004
+  cf.weight_decay = 0.05
   cf.lr_decay_rate = 1.025
   cf.lr_start_epochs = 3
-  cf.lat_sampling_weighted = False
+  cf.lat_sampling_weighted = True
   # BERT
-  cf.BERT_strategy = 'BERT'      # 'BERT', 'forecast', 'identity', 'totalmask'
+  # strategies: 'BERT', 'forecast', 'temporal_interpolation', 'identity'
+  cf.BERT_strategy = 'BERT'     
   cf.BERT_fields_synced = False   # apply synchronized / identical masking to all fields 
                                   # (fields need to have same BERT params for this to have effect)
   cf.BERT_mr_max = 2              # maximum reduction rate for resolution
@@ -209,14 +213,34 @@ def train_multi() :
   cf.log_test_num_ranks = 0
   cf.save_grads = False
   cf.profile = False
-  cf.test_initial = True
+  cf.test_initial = False
   cf.attention = False
 
   cf.rng_seed = None 
 
   # usually use %>wandb offline to switch to disable syncing with server
   cf.with_wandb = True
-  setup_wandb( cf.with_wandb, cf, par_rank, 'train-multi', mode='offline')  
+  setup_wandb( cf.with_wandb, cf, par_rank, 'train', mode='offline')  
+
+  cf.with_mixed_precision = True
+  cf.num_samples_per_epoch = 4096
+  cf.num_samples_validate = 128
+  cf.num_loader_workers = 6
+
+  #cf.file_path = '/p/scratch/atmo-rep/data/era5_1deg/months/era5_y2021_res100_chunk32.zarr'
+  # # in steps x lat_degrees x lon_degrees
+  #cf.n_size = [36, 1*9*6, 1.*9*12]
+
+  # # # cf.file_path = '/p/scratch/atmo-rep/data/era5_1deg/months/era5_y2021_res025_chunk16.zarr'
+  # # cf.file_path = '/p/scratch/atmo-rep/data/era5_1deg/months/era5_y2021_res025_chunk32.zarr'
+  #cf.file_path = '/ec/res4/scratch/nacl/atmorep/era5_y2021_res025_chunk32.zarr'
+  # 
+  #cf.file_path = '/p/scratch/atmo-rep/data/era5_1deg/months/era5_y2021_res025_chunk8.zarr'
+  # cf.file_path = '/ec/res4/scratch/nacl/atmorep/era5_y2021_res025_chunk8_lat180_lon180.zarr'
+  cf.file_path = '/gpfs/scratch/ehpc03/era5_y2010_2021_res025_chunk8.zarr'
+  # cf.file_path = '/ec/res4/scratch/nacl/atmorep/era5_y2021_res025_chunk16.zarr'
+  # in steps x lat_degrees x lon_degrees
+  cf.n_size = [36, 0.25*9*6, 0.25*9*12]
 
   if cf.with_wandb and 0 == cf.par_rank :
     cf.write_json( wandb)
@@ -228,12 +252,10 @@ def train_multi() :
 ####################################################################################################
 if __name__ == '__main__':
 
-  train_multi()
+  train()
 
-  # # Continue training run
-  
-#  model_id, model_epoch = '1jh2qvrx', -2
-#  model_epoch_continue = 0
-#  
+#  wandb_id, epoch = '1jh2qvrx', 392 #'4nvwbetz', -2 #392  #'4nvwbetz', -2
+#  epoch_continue = epoch
+
 #  Trainer = Trainer_BERT
-#  train_continue( model_id, model_epoch, Trainer, model_epoch_continue)
+#  train_continue( wandb_id, epoch, Trainer, epoch_continue)
