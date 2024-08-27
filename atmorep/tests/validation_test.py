43c23e10d5ef675a8c2a5764c11b08839e1349f3
Simon Grasse <s.grasse@fz-juelich.de>
2024-08-27 15:38:44 +0200

    refactor: parametrize class with sampled input instead of individual tests

--- atmorep/tests/validation_test.py
+++ atmorep/tests/validation_test.py
@@ -37,70 +37,64 @@ def levels(config):
 @pytest.mark.parametrize(
     ("sample", "level"), test_utils.ValidationConfig.get().samples_and_levels()
 )
-def test_datetime(sample, level, levels, target, config: test_utils.ValidationConfig):
-    """
-    Check against ERA5 timestamps.
-    Loop over all levels individually. 50 random samples for each level.
-    """
-    level_idx: int = config.data_access.get_level_idx(levels, level)
-    
-    data, datetime, lats, lons = config.data_access.get_data(
-        target,
-        config.field,
-        sample,
-        level_idx,
-    )
-    year, month = datetime.year, str(datetime.month).zfill(2)
+class TestValidateOutput:
+    def test_datetime(self, sample, level, levels, target, config: test_utils.ValidationConfig):
+        """
+        Check against ERA5 timestamps.
+        Loop over all levels individually. 50 random samples for each level.
+        """
+        level_idx: int = config.data_access.get_level_idx(levels, level)
+        
+        data, datetime, lats, lons = config.data_access.get_data(
+            target,
+            config.field,
+            sample,
+            level_idx,
+        )
+        year, month = datetime.year, str(datetime.month).zfill(2)
 
-    era5_path = test_utils.ERA5_FNAME.format(
-        config.field, level, config.field, year, month, level
-    )
-    if not os.path.isfile(era5_path):
-        warnings.warn(UserWarning((f"Timestamp {datetime} not found in ERA5. Skipping")))
-    else:
-        era5 = xr.open_dataset(era5_path, engine = "cfgrib")[
-            test_utils.FIELD_GRIB_IDX[config.field]
-        ].sel(time = datetime, latitude = lats, longitude = lons)
+        era5_path = test_utils.ERA5_FNAME.format(
+            config.field, level, config.field, year, month, level
+        )
+        if not os.path.isfile(era5_path):
+            warnings.warn(UserWarning((f"Timestamp {datetime} not found in ERA5. Skipping")))
+        else:
+            era5 = xr.open_dataset(era5_path, engine = "cfgrib")[
+                test_utils.FIELD_GRIB_IDX[config.field]
+            ].sel(time = datetime, latitude = lats, longitude = lons)
 
-        # assert (data[0] == era5.values[0]).all(), "Mismatch between ERA5 and AtmoRep Timestamps"
-        assert np.isclose(data[0], era5.values[0],rtol=1e-04, atol=1e-07).all(), "Mismatch between ERA5 and AtmoRep Timestamps"
+            # assert (data[0] == era5.values[0]).all(), "Mismatch between ERA5 and AtmoRep Timestamps"
+            assert np.isclose(data[0], era5.values[0],rtol=1e-04, atol=1e-07).all(), "Mismatch between ERA5 and AtmoRep Timestamps"
 
-
-@pytest.mark.parametrize(
-    ("sample", "level"), test_utils.ValidationConfig.get().samples_and_levels()
-)
-def test_coordinates(
-    sample, level, levels, target, prediction, config: test_utils.ValidationConfig
-):
-    """
-    Check that coordinates match between target and prediction. 
-    Check also that latitude and longitudes are in geographical coordinates
-    50 random samples.
-    """
-
-    level_idx: int = config.data_access.get_level_idx(levels, level)
-    _, datetime_target, lats_target, lons_target = config.data_access.get_data(target,config.field, sample, level_idx)
-    _, datetime_pred, lats_pred, lons_pred = config.data_access.get_data(prediction, config.field, sample, level_idx)
-
-    test_utils.test_lats_match(lats_pred, lats_target)
-    test_utils.test_lats_in_range(lats_pred)
-    test_utils.test_lons_match(lons_pred, lons_target)
-    test_utils.test_lons_in_range(lons_pred)
-    test_utils.test_datetimes_match(datetime_pred, datetime_target)
-
-@pytest.mark.parametrize(
-    ("sample", "level"), test_utils.ValidationConfig.get().samples_and_levels()
-)
-def test_rmse(
-    sample, level, levels, target, prediction, config: test_utils.ValidationConfig
+    def test_coordinates(
+        self, sample, level, levels, target, prediction, config: test_utils.ValidationConfig
     ):
-    """
-    Test that for each field the RMSE does not exceed a certain value. 
-    50 random samples.
-    """
+        """
+        Check that coordinates match between target and prediction. 
+        Check also that latitude and longitudes are in geographical coordinates
+        50 random samples.
+        """
 
-    level_idx: int = config.data_access.get_level_idx(levels, level)
-    sample_target, _, _, _ = config.data_access.get_data(target, config.field, sample, level_idx)
-    sample_pred, _, _, _ = config.data_access.get_data(prediction, config.field, sample, level_idx)
+        level_idx: int = config.data_access.get_level_idx(levels, level)
+        _, datetime_target, lats_target, lons_target = config.data_access.get_data(target,config.field, sample, level_idx)
+        _, datetime_pred, lats_pred, lons_pred = config.data_access.get_data(prediction, config.field, sample, level_idx)
 
-    assert test_utils.compute_RMSE(sample_target, sample_pred).mean() < test_utils.FIELD_MAX_RMSE[config.field]
+        test_utils.test_lats_match(lats_pred, lats_target)
+        test_utils.test_lats_in_range(lats_pred)
+        test_utils.test_lons_match(lons_pred, lons_target)
+        test_utils.test_lons_in_range(lons_pred)
+        test_utils.test_datetimes_match(datetime_pred, datetime_target)
+
+    def test_rmse(
+        self, sample, level, levels, target, prediction, config: test_utils.ValidationConfig
+        ):
+        """
+        Test that for each field the RMSE does not exceed a certain value. 
+        50 random samples.
+        """
+
+        level_idx: int = config.data_access.get_level_idx(levels, level)
+        sample_target, _, _, _ = config.data_access.get_data(target, config.field, sample, level_idx)
+        sample_pred, _, _, _ = config.data_access.get_data(prediction, config.field, sample, level_idx)
+
+        assert test_utils.compute_RMSE(sample_target, sample_pred).mean() < test_utils.FIELD_MAX_RMSE[config.field]
