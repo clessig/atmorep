eb22d050580fb29a5c7ceec85c132d8e078f0f52
semcheddine1 <semcheddine1@jwlogin21.juwels>
2024-09-06 11:18:26 +0200

    Asma output scratch folders
    Title: implementing masked timesteps test function
    
    Description:
       - atmorep/tests/validation_test.py : The test function is `test_idx_time_mask()`
       the other functions have been commented to test the only function written by me
       - atmorep/tests/test_utils.py :
            + Helper function `check_masked_timesteps()` which is a duplicate of `check_datetimes()`
            but returns a slightly different message
            +  Helper function `get_datetime_target()`
            +  Helper function `get_datetime_source()`. Because `source` data is accessed slightly
            differently than `target`
       - atmorep/config/config.py : Folders to read from changed according to where my outputs are

--- atmorep/tests/test_utils.py
+++ atmorep/tests/test_utils.py
@@ -9,16 +9,16 @@ def era5_fname():
     return "/p/scratch/atmo-rep/data/era5/new_structure/{}/ml{}/era5_{}_y{}_m{}_ml{}.grib"
 
 def atmorep_pred():
-    return "./results/id{}/results_id{}_epoch{}_pred.zarr"
+    return "/p/scratch/deepacf/semcheddine1/atmorep_temporal_interpolation/results/id{}/results_id{}_epoch{}_pred.zarr"
 
 def atmorep_target():
-    return "./results/id{}/results_id{}_epoch{}_target.zarr"
+    return "/p/scratch/deepacf/semcheddine1/atmorep_temporal_interpolation/results/id{}/results_id{}_epoch{}_target.zarr"
 
 def atmorep_source(): # Asma
-    return "./results/id{}/results_id{}_epoch{}_source.zarr"
+    return "/p/scratch/deepacf/semcheddine1/atmorep_temporal_interpolation/results/id{}/results_id{}_epoch{}_source.zarr"
 
 def atmorep_json(): # Asma
-    return "./results/id{}/model_{}.json"
+    return "/p/scratch/deepacf/semcheddine1/atmorep_temporal_interpolation/results/id{}/model_id{}.json"
 
 def grib_index(field):
     grib_idxs = {"velocity_u": "u",
@@ -42,6 +42,22 @@ def get_BERT(atmorep, field, sample, level):
     lons = atmorep_sample.lon[0]
     return data, datetime, lats, lons
 
+### Asma: new function
+### Asma: will be improved according to comments
+def get_datetime_target(atmorep, field, sample, level, token_size):
+    atmorep_sample = atmorep[f"{field}/sample={sample:05d}/ml={level:05d}"] 
+    datetime = np.unique(atmorep_sample.datetime[:])
+    datetime = datetime.reshape(-1, token_size)
+    return datetime
+
+### Asma: new function
+### Asma: will be improved according to comments
+def get_datetime_source(atmorep, field, sample, token_size):
+    atmorep_sample = atmorep[f"{field}/sample={sample:05d}"] 
+    datetime = atmorep_sample.datetime[:]
+    datetime = datetime.reshape(-1, token_size)
+    return datetime
+
 def get_forecast(atmorep, field, sample,level_idx):
     atmorep_sample = atmorep[f"{field}/sample={sample:05d}"]
     data = atmorep_sample.data[level_idx, 0]
@@ -49,16 +65,6 @@ def get_forecast(atmorep, field, sample,level_idx):
     lats = atmorep_sample.lat
     lons = atmorep_sample.lon
     return data, datetime, lats, lons
-
-# Asma: I think this is useless
-# def get_temporal_interpolation(atmorep, field, sample,level_idx, idx_time_mask):
-#     atmorep_sample = atmorep[f"{field}/sample={sample:05d}"]
-#     data = atmorep_sample.data[level_idx, 0]
-#     datetime = pd.Timestamp(atmorep_sample.datetime[0])
-#     lats = atmorep_sample.lat
-#     lons = atmorep_sample.lon
-#     return data, datetime, lats, lons
-
 
 ######################################
 
@@ -75,18 +81,14 @@ def check_lons(lons_pred, lons_target):
 def check_datetimes(datetimes_pred, datetimes_target):
     assert (datetimes_pred == datetimes_target), "Mismatch between datetimes"
 
-def check_masked_timesteps(datetime_target, datetime_source, idx_time_mask):
-    masked_source_timesteps = datetime_source[idx_time_mask]
-    assert (masked_source_timesteps == datetime_target), "Mismatch between masked datetimes"
-
-def check_masked_timesteps2(datetime_target, datetime_source, idx_time_mask):
-    idx_time_mask_list = [1, 2, 3] # ast.literal_eval(idx_time_mask)
-    masked_source_timesteps = datetime_source[idx_time_mask]
-    assert (masked_source_timesteps == datetime_target), "Mismatch between masked datetimes"
+# Asma: keep new function or use check_datetimes() instead?
+def check_masked_timesteps(datetime_target, datetime_source):
+    assert (datetime_target == datetime_source), "Mismatch between masked datetimes"
 
 # Asma: TODO
 # def check_masked_data(data_target, data_source, idx_time_mask):
-#     return 0
+### indexes of source should match idx_time_mask from options (if masking is done by putting data points to zeros)
+### or assert target to ERA5 at the masked idxes
 
 ######################################
 
--- atmorep/tests/validation_test.py
+++ atmorep/tests/validation_test.py
@@ -41,104 +41,102 @@ def temporal_interpolation(request): # Asma
 
 #TODO: add test for global_forecast vs ERA5
 
-def test_datetime(field, model_id, BERT, epoch = 0):
+# def test_datetime(field, model_id, BERT, epoch = 0):
 
-    """
-    Check against ERA5 timestamps.
-    Loop over all levels individually. 50 random samples for each level.
-    """
+#     """
+#     Check against ERA5 timestamps.
+#     Loop over all levels individually. 50 random samples for each level.
+#     """
 
-    store = zarr.ZipStore(atmorep_target().format(model_id, model_id, str(epoch).zfill(5)))
-    atmorep = zarr.group(store)
+#     store = zarr.ZipStore(atmorep_target().format(model_id, model_id, str(epoch).zfill(5)))
+#     atmorep = zarr.group(store)
 
-    nsamples = min(len(atmorep[field]), 50)
-    samples = rnd.sample(range(len(atmorep[field])), nsamples)
-    levels = [int(f.split("=")[1]) for f in atmorep[f"{field}/sample=00000"]] if BERT else atmorep[f"{field}/sample=00000"].ml[:]
+#     nsamples = min(len(atmorep[field]), 50)
+#     samples = rnd.sample(range(len(atmorep[field])), nsamples)
+#     levels = [int(f.split("=")[1]) for f in atmorep[f"{field}/sample=00000"]] if BERT else atmorep[f"{field}/sample=00000"].ml[:]
    
-    get_data = get_BERT if BERT else get_forecast
+#     get_data = get_BERT if BERT else get_forecast
    
-    for level in levels:
-        #TODO: make it more elegant
-        level_idx = level if BERT else np.where(levels == level)[0].tolist()[0]
+#     for level in levels:
+#         #TODO: make it more elegant
+#         level_idx = level if BERT else np.where(levels == level)[0].tolist()[0]
 
-        for s in samples:
-            data, datetime, lats, lons = get_data(atmorep, field, s, level_idx)
-            year, month = datetime.year, str(datetime.month).zfill(2)
+#         for s in samples:
+#             data, datetime, lats, lons = get_data(atmorep, field, s, level_idx)
+#             year, month = datetime.year, str(datetime.month).zfill(2)
 
-            era5_path = era5_fname().format(field, level, field, year, month, level)
-            if not os.path.isfile(era5_path):
-                warnings.warn(UserWarning((f"era5_path = {era5_path} Timestamp {datetime} not found in ERA5. Skipping")))
-                continue
-            era5 = xr.open_dataset(era5_path, engine = "cfgrib")[grib_index(field)].sel(time = datetime, latitude = lats, longitude = lons)
+#             era5_path = era5_fname().format(field, level, field, year, month, level)
+#             if not os.path.isfile(era5_path):
+#                 warnings.warn(UserWarning((f"era5_path = {era5_path} Timestamp {datetime} not found in ERA5. Skipping")))
+#                 continue
+#             era5 = xr.open_dataset(era5_path, engine = "cfgrib")[grib_index(field)].sel(time = datetime, latitude = lats, longitude = lons)
 
-            #assert (data[0] == era5.values[0]).all(), "Mismatch between ERA5 and AtmoRep Timestamps"
-            assert np.isclose(data[0], era5.values[0],rtol=1e-04, atol=1e-07).all(), "Mismatch between ERA5 and AtmoRep Timestamps"
+#             #assert (data[0] == era5.values[0]).all(), "Mismatch between ERA5 and AtmoRep Timestamps"
+#             assert np.isclose(data[0], era5.values[0],rtol=1e-04, atol=1e-07).all(), "Mismatch between ERA5 and AtmoRep Timestamps"
 
 #############################################################################
 
-def test_coordinates(field, model_id, BERT, epoch = 0):
-    """
-    Check that coordinates match between target and prediction. 
-    Check also that latitude and longitudes are in geographical coordinates
-    50 random samples.
-    """
+# def test_coordinates(field, model_id, BERT, epoch = 0):
+#     """
+#     Check that coordinates match between target and prediction. 
+#     Check also that latitude and longitudes are in geographical coordinates
+#     50 random samples.
+#     """
    
-    store_t = zarr.ZipStore(atmorep_target().format(model_id, model_id, str(epoch).zfill(5)))
-    target = zarr.group(store_t)
+#     store_t = zarr.ZipStore(atmorep_target().format(model_id, model_id, str(epoch).zfill(5)))
+#     target = zarr.group(store_t)
 
-    store_p = zarr.ZipStore(atmorep_pred().format(model_id, model_id, str(epoch).zfill(5)))
-    pred = zarr.group(store_p)
+#     store_p = zarr.ZipStore(atmorep_pred().format(model_id, model_id, str(epoch).zfill(5)))
+#     pred = zarr.group(store_p)
 
-    nsamples = min(len(target[field]), 50)
-    samples = rnd.sample(range(len(target[field])), nsamples)
-    levels = [int(f.split("=")[1]) for f in target[f"{field}/sample=00000"]] if BERT else target[f"{field}/sample=00000"].ml[:]
+#     nsamples = min(len(target[field]), 50)
+#     samples = rnd.sample(range(len(target[field])), nsamples)
+#     levels = [int(f.split("=")[1]) for f in target[f"{field}/sample=00000"]] if BERT else target[f"{field}/sample=00000"].ml[:]
    
-    get_data = get_BERT if BERT else get_forecast
+#     get_data = get_BERT if BERT else get_forecast
 
-    for level in levels:
-        level_idx = level if BERT else np.where(levels == level)[0].tolist()[0]
-        for s in samples:
-            _, datetime_target, lats_target, lons_target = get_data(target,field, s, level_idx)
-            _, datetime_pred, lats_pred, lons_pred = get_data(pred, field, s, level_idx)
+#     for level in levels:
+#         level_idx = level if BERT else np.where(levels == level)[0].tolist()[0]
+#         for s in samples:
+#             _, datetime_target, lats_target, lons_target = get_data(target,field, s, level_idx)
+#             _, datetime_pred, lats_pred, lons_pred = get_data(pred, field, s, level_idx)
 
-            check_lats(lats_pred, lats_target)
-            check_lons(lons_pred, lons_target)
-            check_datetimes(datetime_pred, datetime_target)
+#             check_lats(lats_pred, lats_target)
+#             check_lons(lons_pred, lons_target)
+#             check_datetimes(datetime_pred, datetime_target)
 
 #########################################################################
 
-def test_rmse(field, model_id, BERT, epoch = 0):
-    """
-    Test that for each field the RMSE does not exceed a certain value. 
-    50 random samples.
-    """
-    store_t = zarr.ZipStore(atmorep_target().format(model_id, model_id, str(epoch).zfill(5)))
-    target = zarr.group(store_t)
+# def test_rmse(field, model_id, BERT, epoch = 0):
+#     """
+#     Test that for each field the RMSE does not exceed a certain value. 
+#     50 random samples.
+#     """
+#     store_t = zarr.ZipStore(atmorep_target().format(model_id, model_id, str(epoch).zfill(5)))
+#     target = zarr.group(store_t)
 
-    store_p = zarr.ZipStore(atmorep_pred().format(model_id, model_id, str(epoch).zfill(5)))
-    pred = zarr.group(store_p)
+#     store_p = zarr.ZipStore(atmorep_pred().format(model_id, model_id, str(epoch).zfill(5)))
+#     pred = zarr.group(store_p)
     
-    nsamples = min(len(target[field]), 50)
-    samples = rnd.sample(range(len(target[field])), nsamples)
-    levels = [int(f.split("=")[1]) for f in target[f"{field}/sample=00000"]] if BERT else target[f"{field}/sample=00000"].ml[:]
+#     nsamples = min(len(target[field]), 50)
+#     samples = rnd.sample(range(len(target[field])), nsamples)
+#     levels = [int(f.split("=")[1]) for f in target[f"{field}/sample=00000"]] if BERT else target[f"{field}/sample=00000"].ml[:]
     
-    get_data = get_BERT if BERT else get_forecast
+#     get_data = get_BERT if BERT else get_forecast
     
-    for level in levels:
-        level_idx = level if BERT else np.where(levels == level)[0].tolist()[0]
-        for s in samples:
-            sample_target, _, _, _ = get_data(target,field, s, level_idx)
-            sample_pred, _, _, _ = get_data(pred,field, s, level_idx)
+#     for level in levels:
+#         level_idx = level if BERT else np.where(levels == level)[0].tolist()[0]
+#         for s in samples:
+#             sample_target, _, _, _ = get_data(target,field, s, level_idx)
+#             sample_pred, _, _, _ = get_data(pred,field, s, level_idx)
 
-            assert compute_RMSE(sample_target, sample_pred).mean() < get_max_RMSE(field)
+#             assert compute_RMSE(sample_target, sample_pred).mean() < get_max_RMSE(field)
 
 ##################################################################################
 # Asma c:
-
-# def test_target_vs_era5(field, model_id, BERT, epoch = 0):
-#     print(f"Hello world!")
-#     return 0
-
+### Asma: this part was changed
+### Asma: will be improved depending on comments
+        
 def test_idx_time_mask(field, model_id, BERT, epoch = 0):
 
     """
@@ -152,25 +150,29 @@ def test_idx_time_mask(field, model_id, BERT, epoch = 0):
         store_s = zarr.ZipStore(atmorep_source().format(model_id, model_id, str(epoch).zfill(5)))
         source = zarr.group(store_s)
 
-        nsamples = min(len(atmorep_target[field]), 5)
-        samples = rnd.sample(range(len(atmorep_target[field])), nsamples)
-        levels = [int(f.split("=")[1]) for f in atmorep_target[f"{field}/sample=00000"]]
+        nsamples = min(len(target[field]), 5)
+        samples = rnd.sample(range(len(target[field])), nsamples)
+        levels = [int(f.split("=")[1]) for f in target[f"{field}/sample=00000"]]
 
         with open(atmorep_json().format(model_id, model_id), 'r') as file_model_json:
             model_json = json.load(file_model_json)
             
         idx_time_mask = model_json['idx_time_mask']
-        warnings.warn(UserWarning((f"idx_time_mask is {idx_time_mask} and is of type {type(idx_time_mask)}")))
-    # levels = [int(f.split("=")[1]) for f in atmorep_target[f"{field}/sample=00000"]] if BERT else atmorep_target[f"{field}/sample=00000"].ml[:]
-        # get_data = get_BERT # Asma: useless
-        for level in levels:
-            level_idx = level
-            for s in samples:
-                data_target, datetime_target, lats_target, lons_target = get_BERT(target,field, s, level_idx)
-                data_source, datetime_source, lats_source, lons_source= get_BERT(source, field, s, level_idx)
+        token_size = 3 # to be improved
 
-                check_masked_timesteps(datetime_target, datetime_source, idx_time_mask)
-                check_masked_timesteps2(datetime_target, datetime_source, idx_time_mask)
-                # check_masked_data(data_target, data_source, idx_time_mask)
+        for s in samples:
+            datetime_source = get_datetime_source(source,field, s, token_size)
+            masked_source_timesteps = datetime_source[idx_time_mask]
+            masked_source_timesteps = masked_source_timesteps.flatten()
+            for level in levels:
+                datetime_target = get_datetime_target(target,field, s, level, token_size)
+                datetime_target = datetime_target.flatten()
+
+                for timestep_idx in range(len(datetime_target)):
+                    check_masked_timesteps(datetime_target[timestep_idx], masked_source_timesteps[timestep_idx])
+
     else:
         warnings.warn(UserWarning((f"Strategy is {strategy}")))
+
+    # Asma: TODO
+    # def test_idx_masked_data(field, model_id, BERT, epoch = 0):
