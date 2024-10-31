cf64db90d19716e6fa4926abf5840c9d54f08b9b
Javad Kasravi <j.kasravi@fz-juelich.de>
2024-10-31 11:59:27 +0100

    remove torch package

--- atmorep/core/train.py
+++ atmorep/core/train.py
@@ -208,6 +208,8 @@ def train() :
   cf.with_wandb = True
   setup_wandb( cf.with_wandb, cf, par_rank, 'train', mode='offline')  
 
+  cf.file_path = '/p/scratch/atmo-rep/data/era5_1deg/months/era5_y1979_2021_res025.zarr'
+
   # cf.file_path = '/p/scratch/atmo-rep/data/era5_1deg/months/era5_y2021_res100_chunk32.zarr'
   # cf.file_path = '/ec/res4/scratch/nacl/atmorep/era5_y2021_res100_chunk32.zarr'
   # # # in steps x lat_degrees x lon_degrees
@@ -220,7 +222,7 @@ def train() :
   # # # cf.file_path = '/p/scratch/atmo-rep/data/era5_1deg/months/era5_y2021_res025_chunk8.zarr'
   # # cf.file_path = '/ec/res4/scratch/nacl/atmorep/era5_y2021_res025_chunk8_lat180_lon180.zarr'
   # # # cf.file_path = '/ec/res4/scratch/nacl/atmorep/era5_y2021_res025_chunk16.zarr'
-  cf.file_path = '/gpfs/scratch/ehpc03/era5_y2010_2021_res025_chunk8.zarr/'
+  #cf.file_path = '/gpfs/scratch/ehpc03/era5_y2010_2021_res025_chunk8.zarr/'
   # # # in steps x lat_degrees x lon_degrees
   cf.n_size = [36, 0.25*9*6, 0.25*9*12]
 
