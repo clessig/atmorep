260438de40d0bdb813a5fb9d0102019c737c6d7a
Michael <m.langguth@fz-juelich.de>
2024-11-06 16:40:08 +0100

    Add scripts for setting up virtual env on JSC clusters.

---
+++ jsc_scripts/env_setup/create_env.sh
@@ -0,0 +1,140 @@
+#!/usr/bin/env bash
+#
+# __authors__ = Michael Langguth
+# __date__  = '2022-01-21'
+# __update__= '2024-11-06'
+#
+# **************** Description ****************
+# This script can be used for setting up the virtual environment needed for AtmoRep.
+# **************** Description ****************
+#
+### auxiliary-function S ###
+check_argin() {
+# Handle input arguments and check if one is equal to -lcontainer (not needed currently)
+# Can also be used to check for non-positional arguments (such as -exp_id=*, see commented lines)
+# !!! NOT USED YET!!!
+    for argin in "$@"; do
+        # if [[ $argin == *"-exp_id="* ]]; then
+        #  exp_id=${argin#"-exp_id="}
+        if [[ $argin == *"-lcontainer"* ]]; then
+	        bool_container=1
+        fi  
+    done
+    if [[ -z "${bool_container}" ]]; then
+        bool_container=0
+    fi
+}
+### auxiliary-function E ###
+
+### MAIN S ###
+#set -eu              # enforce abortion if a command is not re
+
+SCR_SETUP="%create_env.sh: "
+
+## some first sanity checks
+# script is sourced?
+if [[ ${BASH_SOURCE[0]} == "${0}" ]]; then
+  echo "${SCR_SETUP}ERROR: 'create_env.sh' must be sourced, i.e. execute by prompting 'source create_env.sh [virt_env_name]'"
+  exit 1
+fi
+
+
+# from now on, just return if something unexpected occurs instead of exiting
+# as the latter would close the terminal including logging out
+if [[ -z "$1" ]]; then
+  echo "${SCR_SETUP}ERROR: Provide a name to set up the virtual environment, i.e. execute by prompting 'source create_env.sh [virt_env_name]"
+  return
+fi
+
+# set some variables
+HOST_NAME=$(hostname)
+ENV_NAME=$1
+SETUP_DIR=$(pwd)
+SETUP_DIR_NAME="$(basename "${SETUP_DIR}")"
+BASE_DIR="$(dirname "${SETUP_DIR}")"
+VENV_DIR="${BASE_DIR}/virtual_envs/${ENV_NAME}"
+ATMOREP_DIR="$(dirname "${BASE_DIR}")"
+
+## perform sanity checks
+# * check if script is called from env_setup-directory
+# * check if virtual env has already been set up
+
+# script is called from env_setup-directory?
+if [[ "${SETUP_DIR_NAME}" != "env_setup"  ]]; then
+  echo "${SCR_SETUP}ERROR: Execute 'create_env.sh' from the env_setup-subdirectory only!"
+  echo "${SETUP_DIR_NAME}"
+  return
+fi
+
+# virtual environment already set-up?
+if [[ -d ${VENV_DIR} ]]; then
+  echo "${SCR_SETUP}Virtual environment has already been set up under ${VENV_DIR} and is ready to use."
+  echo "NOTE: If you wish to set up a new virtual environment, delete the existing one or provide a different name."
+  ENV_EXIST=1
+else
+  ENV_EXIST=0
+fi
+
+## check integratability of operating system
+if [[ "${HOST_NAME}" == *jrlogin* || "${HOST_NAME}" == *jwlogin* ]]; then
+  # unset PYTHONPATH to ensure that system-realted paths are not set
+  unset PYTHONPATH
+  modules_file="modules_jsc.sh"
+else
+  echo "${SCR_SETUP}ERROR: Software stack can only be set-up for JURECA and Juwels (Booster) so far."
+  return
+fi
+
+## set up virtual environment
+
+activate_virt_env=${VENV_DIR}/bin/activate
+
+if [[ "$ENV_EXIST" == 0 ]]; then
+  # Install virtualenv-package and set-up virtual environment with required additional Python packages.
+  echo "${SCR_SETUP}Configuring and activating virtual environment on ${HOST_NAME}"
+
+  source "${modules_file}"
+
+  python3 -m venv --system-site-packages "${VENV_DIR}"
+
+  echo "${SCR_SETUP}Entering virtual environment ${VENV_DIR} to install required Python modules..."
+  source "${activate_virt_env}"
+ 
+  # handle systematic issues with Stages/2022 
+  MACHINE=$(hostname -f | cut -d. -f2)
+  if [[ "${HOST}" == jwlogin2[2-4] ]]; then
+     MACHINE="juwelsbooster"
+  fi
+  PY_VERSION=$(python --version 2>&1 | cut -d ' ' -f2 | cut -d. -f1-2)
+
+  echo "${SCR_SETUP}Appending PYTHONPATH on ${MACHINE} for Python version ${PY_VERSION} to ensure proper set-up..."
+
+  req_file=${SETUP_DIR}/requirements.txt
+
+  # Install additional requirements
+  pip3 install --no-cache-dir -r "${req_file}"
+  # Install atmorep and its dependencies
+  pip3 install -e ../../
+
+  # expand PYTHONPATH
+  ##export PYTHONPATH=${ATMOREP_DIR}:$PYTHONPATH >> "${activate_virt_env}"
+
+  ## ...and ensure that this also done when the
+  #echo "" >> "${activate_virt_env}"
+  #echo "# Expand PYTHONPATH..." >> "${activate_virt_env}"
+  #echo "export PYTHONPATH=${ATMOREP_DIR}:\$PYTHONPATH" >> "${activate_virt_env}"
+
+  # deactivate virtual environment
+  # NOTE: virtual environment must be deactivated when submitting batch-scripts to avoid issues
+  deactivate
+
+  info_str="Virtual environment ${VENV_DIR} has been set up successfully."
+elif [[ "$ENV_EXIST" == 1 ]]; then
+  # simply activate virtual environment
+  info_str="Virtual environment ${VENV_DIR} has already been set up before. Nothing to be done."
+  
+  source "${activate_virt_env}"
+fi
+
+echo "${SCR_SETUP}${info_str}"
+### MAIN E ###
---
+++ jsc_scripts/env_setup/modules_jsc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ml --force purge
+ml use $OTHERSTAGES
+ml Stages/2024
+
+ml CUDA 
+
+ml GCC/12.3.0
+ml GCCcore/.12.3.0
+
+ml OpenMPI/4.1.5
+
+ml netcdf4-python/1.6.4
+ml SciPy-bundle/2023.07
+ml matplotlib/3.7.2
+ml xarray/2023.8.0
+ml dask/2023.9.2
+ml ecCodes/2.31.0
+ml zarr/2.18.3
+ml PyYAML/6.0
