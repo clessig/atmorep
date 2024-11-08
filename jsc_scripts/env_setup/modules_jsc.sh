cc5a13d33fbd89299c35ff6b90e8f7a29ea23d07
Michael <m.langguth@fz-juelich.de>
2024-11-08 15:39:21 +0100

    Bug:Use serial netcdf4-python-version when creating virtual envs since the non-serial version raises MPI-errors.

--- jsc_scripts/env_setup/modules_jsc.sh
+++ jsc_scripts/env_setup/modules_jsc.sh
@@ -10,7 +10,6 @@ ml GCCcore/.12.3.0
 
 ml OpenMPI/4.1.5
 
-ml netcdf4-python/1.6.4
 ml SciPy-bundle/2023.07
 ml matplotlib/3.7.2
 ml xarray/2023.8.0
@@ -18,3 +17,4 @@ ml dask/2023.9.2
 ml ecCodes/2.31.0
 ml zarr/2.18.3
 ml PyYAML/6.0
+ml netcdf4-python/1.6.4-serial
