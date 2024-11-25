c0dc2eaa25671f8372eac6208bcea294670d8023
semcheddine1 <semcheddine1@jwlogin24.juwels>
2024-11-25 12:46:28 +0100

    helper functions used in temporal interpolation tests

--- atmorep/tests/test_utils.py
+++ atmorep/tests/test_utils.py
@@ -10,6 +10,12 @@ def atmorep_pred():
 def atmorep_target():
     return "./results/id{}/results_id{}_epoch{}_target.zarr"
 
+def atmorep_source():
+    return "./results/id{}/results_id{}_epoch{}_source.zarr"
+
+def atmorep_json():
+    return "./results/id{}/model_id{}.json"
+
 def grib_index(field):
     grib_idxs = {"velocity_u": "u",
                  "temperature": "t", 
@@ -39,6 +45,14 @@ def get_forecast(atmorep, field, sample,level_idx):
     lats = atmorep_sample.lat
     lons = atmorep_sample.lon
     return data, datetime, lats, lons
+
+def get_datetime(atmorep, field, sample, BERT=None, level=None):
+    if BERT:
+        atmorep_sample = atmorep[f"{field}/sample={sample:05d}/ml={level:05d}"] 
+        return np.unique(atmorep_sample.datetime[:])
+    else:
+        atmorep_sample = atmorep[f"{field}/sample={sample:05d}"] 
+        return atmorep_sample.datetime[:]
 
 ######################################
 
