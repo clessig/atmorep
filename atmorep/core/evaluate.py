37c24840323f57b3899eb7632de2ff7579b64ef1
Patnala,Ankit <a.patnala@fz-juelich.de>
2025-01-09 13:42:12 +0100

    added lat_lon_domain in the global_forecat options and wrote two more model_id for total_precip and mulitformer

--- atmorep/core/evaluate.py
+++ atmorep/core/evaluate.py
@@ -35,7 +35,9 @@ if __name__ == '__main__':
   #model_id='0tlnm5up' #velocity_v
   #model_id='v63l01zu' #specific humidity 
   #model_id='9l1errbo' #velocity_z
-  model_id='7ojls62c' #temperature 1024 
+  #model_id='7ojls62c' #temperature 1024 
+  #model_id='599ioq5r' #total_precipitation
+  model_id='wc5e2i3t' #multiformer
   
   # supported modes: test, forecast, fixed_location, temporal_interpolation, global_forecast,
   #                  global_forecast_range
@@ -58,17 +60,20 @@ if __name__ == '__main__':
   mode, options = 'global_forecast', { 
                                       #'dates' : [[2021, 2, 10, 12]]
                                       'dates' : [
-                                         [2021, 1, 10, 12] , [2021, 1, 11, 0], [2021, 1, 11, 12], [2021, 1, 12, 0], #[2021, 1, 12, 12], [2021, 1, 13, 0], 
-                                         [2021, 4, 10, 12], [2021, 4, 11, 0], [2021, 4, 11, 12], [2021, 4, 12, 0], #[2021, 4, 12, 12], [2021, 4, 13, 0], 
-                                         [2021, 7, 10, 12], [2021, 7, 11, 0], [2021, 7, 11, 12], [2021, 7, 12, 0], #[2021, 7, 12, 12], [2021, 7, 13, 0], 
-                                         [2021, 10, 10, 12], [2021, 10, 11, 0], [2021, 10, 11, 12], #[2021, 10, 12, 0], [2021, 10, 12, 12], [2021, 10, 13, 0]
+                                         [2020, 1, 10, 12] , [2020, 1, 11, 0], [2020, 1, 11, 12], [2020, 1, 12, 0], #[2021, 1, 12, 12], [2021, 1, 13, 0], 
+                                         [2020, 4, 10, 12], [2020, 4, 11, 0], [2020, 4, 11, 12], [2020, 4, 12, 0], #[2021, 4, 12, 12], [2021, 4, 13, 0], 
+                                         
+                                         [2020, 7, 10, 12], [2020, 7, 11, 0], [2020, 7, 11, 12], [2020, 7, 12, 0], #[2021, 7, 12, 12], [2021, 7, 13, 0], 
+                                         [2020, 10, 10, 12], [2020, 10, 11, 0], [2020, 10, 11, 12], #[2021, 10, 12, 0], [2021, 10, 12, 12], [2021, 10, 13, 0]
                                        ], 
                                       'token_overlap' : [0, 0],
                                       'forecast_num_tokens' : 2,
-                                      'with_pytest' : True }
+                                      'lat_lon_domain' : ([16.25,65.0],[341.,35.]),
+                                      'with_pytest' : False }
 
-  file_path = '/gpfs/scratch/ehpc03/era5_y1979_2021_res025_chunk8.zarr'
-  
+  #file_path = '/gpfs/scratch/ehpc03/era5_y1979_2021_res025_chunk8.zarr'
+  file_path = "/p/scratch/atmo-rep/data/era5_1deg/months/era5_y1979_2021_res025_chunk8.zarr/"  
+
   now = time.time()
   Evaluator.evaluate( mode, model_id, file_path, options)
   print("time", time.time() - now)
